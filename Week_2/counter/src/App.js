bcb09c3c3a0a6e0bd5a0ee098e0d9bf0a3ed52d5
Otulugbu Alexander <s2200742@Alexander-MacBook-Air.local>
2022-10-26 14:14:21 +0300

    Finished counter app

--- Week_2/counter/src/App.js
+++ Week_2/counter/src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 import './Header.css';
 import './Main.css';
 import './Footer.css';
-
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 
 
 
+
 const App = () => {
   return (
     <div>
-      <Header />
+      <Header name="Alexander" />
       <Main />
       <Footer />
     </div>
--- Week_2/counter/src/Main.js
+++ Week_2/counter/src/Main.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
+import Button from './UI/Button';
 
 class Main extends  Component {
     state = {
-        counter: 100,
+        counter: 0,
+        styles: { backgroundColor: 'white' },
     };
 
+    
     addOneHandler = () => {
-        this.setState({counter: this.state.counter + 1});
+        this.setState({counter: this.state.counter +1});
     };
+
+    addFiveHandler = () => {
+        this.setState({counter: this.state.counter +5});
+    };
+
+    resetHandler = () => {
+        this.setState({counter: this.state.counter = 0});
+    };
+
+    removeOneHandler = () => {
+        if (this.state.counter < 0) {
+            this.setState({counter: this.state.counter = 0});
+        } else if (this.state.counter > 0) {
+            this.setState({counter: this.state.counter - 1});
+        } 
+    };
+
+    removeFiveHandler = () => {
+        if (this.state.counter < 0) {
+            this.setState({counter: this.state.counter = 0});
+        } else if (this.state.counter > 0) {
+            this.setState({counter: this.state.counter - 5});
+        } 
+    };
+
+
     render () {
+        let circleClass = "";
+
+        this.state.counter === 0
+        ? (circleClass = "circle")
+        : (this.state.counter % 2 === 0)
+        ? (circleClass = "circle even")
+        : (circleClass = "circle odd");
     return (
         <main>
             <div className='counter-display'>
-            <h1 className='counter'>{this.state.counter}</h1>
+            <h1 className={circleClass}>{this.state.counter}</h1>
             </div>
             <div className='btn-display'>
-            <button className='btn'>Add Five</button>
+                <Button click={this.addFiveHandler}>+5</Button>
+                <Button click={this.addOneHandler}>+1</Button>
+                <Button click={this.resetHandler}>Reset</Button>
+                <Button click={this.removeFiveHandler}>-5</Button>
+                <Button click={this.removeOneHandler}>-1</Button>
+
+            {/* <button onClick={this.addFiveHandler} className='btn'>Add Five</button>
             <button onClick={this.addOneHandler} className='btn'>Add One</button>
-            <button className='btn'>Reset</button>
-            <button className='btn'>Remove One</button>
-            <button className='btn'>Remove Five</button>
+            <button onClick={this.resetHandler}   className='btn'>Reset</button>
+            <button onClick={this.removeOneHandler} className='btn'>Remove One</button>
+            <button onClick={this.removeFiveHandler} className='btn'>Remove Five</button>  */}
             </div>
         </main>
     );
